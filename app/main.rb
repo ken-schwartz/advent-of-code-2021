cbd41cbbaa71440352d14c43f68a130cac9a0db6
Ken Schwartz <ken.schwartz@beam.dental>
2021-12-02 23:54:40 -0500

    day 2 part 1

--- app/main.rb
+++ app/main.rb
@@ -7,8 +7,21 @@ def count_depth_increases(measurements)
     .reduce(0) { |count, _array| count + 1 }
 end
 
-file = File.open('./files/day_1.txt')
+def determine_position(movements)
+  movements.each_with_object({ position: 0, depth: 0 }) do |movement, hash|
+    direction, distance = movement.split
+
+    case direction.to_sym
+    when :forward then hash[:position] += distance.to_i
+    when :down then hash[:depth] += distance.to_i
+    when :up then hash[:depth] -= distance.to_i
+    end
+  end
+end
+
+file = File.open('./files/day_2.txt')
 file_data = file.readlines.map(&:chomp)
 file.close
 
-puts count_depth_increases(file_data)
+coordinates = determine_position(file_data)
+puts "Coordinates: #{coordinates}, Multiplied: #{coordinates[:position] * coordinates[:depth]}"
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -12,3 +12,18 @@ describe '#count_depth_increases' do
     expect(count_depth_increases(input)).to eq(7)
   end
 end
+
+describe '#determine_position' do
+  let(:movements) do
+    ['forward 5',
+     'down 5',
+     'forward 8',
+     'up 3',
+     'down 8',
+     'forward 2']
+  end
+
+  it 'returns an x,y coordinate based on the movements' do
+    expect(determine_position(movements)).to eq({ position: 15, depth: 10 })
+  end
+end
